a1fb33c9f53a6db2031951aa48d7422f73f3d4d0
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2023-06-02 11:46:31 -0400

    updates

--- plotIntegratedAngularKineticEnergyVsTime.py
+++ plotIntegratedAngularKineticEnergyVsTime.py
@@ -16,8 +16,8 @@ ID = '1D_M1.4_Mdot0.3_Rs180'
 field = 'AngularKineticEnergy'
 
 # Scale of colorbar
-zScale = None
-#zScale = 'log'
+#zScale = None
+zScale = 'log'
 
 saveFigAs = '/home/kkadoogan/fig.{:}.png'.format( ID )
 
@@ -39,7 +39,19 @@ plotfileDirectoryNR = rootDirectory + 'NR' + ID + '/'
 plotfileArrayNR = GetFileArray( plotfileDirectoryNR, plotfileBaseNameNR )
 plotfileArrayNR = np.copy( plotfileArrayNR[:-1] )
 
-nSS = plotfileArrayNR.shape[0]
+dummy, dataUnits, \
+X1, X2, X3, dX1, dX2, dX3, xL, xH, nX \
+  = GetData( plotfileDirectoryGR, plotfileBaseNameGR, 'GF_SqrtGm', \
+             'spherical', True, \
+             ReturnTime = False, ReturnMesh = True )
+nX2 = nX[1]
+nT = 5
+ind = np.empty( nT, dtype = np.int64 )
+ind[0] = 0
+ind[1] = nX2 // 4
+ind[2] = nX2 // 2
+ind[3] = 3 * nX2 // 4
+ind[4] = -1
 
 def Integrate( plotfileDirectory, plotfileBaseName, plotfile ):
     sqrtGm, dataUnits, \
@@ -56,49 +68,61 @@ def Integrate( plotfileDirectory, plotfileBaseName, plotfile ):
                  ReturnTime = True, ReturnMesh = True )
 
     integral = 0.0
-    for iX1 in range( nX[0] ):
-        for iX2 in range( nX[1] ):
-            integral += 2.0 * np.pi * data[iX1,iX2,0] * sqrtGm[iX1,iX2,0] \
-                          * dX1[iX1,iX2,0] * dX2[iX1,iX2,0]
+    for i in range( nT ):
+        for iX1 in range( nX[0] ):
+            integral[i] += data[iX1,ind[i],0] * sqrtGm[iX1,ind[i],0] \
+                             * dX1[iX1,ind[i],0] / np.sin( X2[iX1,ind[i],0] )
 
     return Time, integral
 
-time = np.empty( nSS, np.float64 )
-GR   = np.empty( nSS, np.float64 )
-NR   = np.empty( nSS, np.float64 )
-for iSS in range( nSS ):
-    plotfileGR = plotfileDirectoryGR + plotfileArrayGR[iSS]
-    print( '  {:}/{:}'.format( iSS, nSS ) )
-    time[iSS], GR[iSS] \
-      = Integrate( plotfileDirectoryGR, plotfileBaseNameGR, plotfileGR )
-
-    plotfileNR = plotfileDirectoryNR + plotfileArrayNR[iSS]
-    t, NR[iSS] \
-      = Integrate( plotfileDirectoryNR, plotfileBaseNameNR, plotfileNR )
-
-np.savetxt( 'GR.dat', np.vstack( ( time, GR ) ) )
-np.savetxt( 'NR.dat', np.vstack( ( time, NR ) ) )
-
-time, GR = np.loadtxt( 'GR.dat' )
-time, NR = np.loadtxt( 'NR.dat' )
+#nSS = plotfileArrayNR.shape[0]
+#time = np.empty( nSS, np.float64 )
+#GR   = np.empty( (nT,nSS), np.float64 )
+#NR   = np.empty( (nT,nSS), np.float64 )
+#for iSS in range( nSS ):
+#
+#    print( '  {:}/{:}'.format( iSS, nSS ) )
+#
+#    plotfileGR = plotfileDirectoryGR + plotfileArrayGR[iSS]
+#    time[iSS], GR[:,iSS] \
+#      = Integrate( plotfileDirectoryGR, plotfileBaseNameGR, plotfileGR )
+#
+#    plotfileNR = plotfileDirectoryNR + plotfileArrayNR[iSS]
+#    t, NR[:,iSS] \
+#      = Integrate( plotfileDirectoryNR, plotfileBaseNameNR, plotfileNR )
+#
+#for i in range( nT ):
+#    np.savetxt( 'GR_{:}.dat'.format( i ), np.vstack( ( time, GR[i] ) ) )
+#    np.savetxt( 'NR_{:}.dat'.format( i ), np.vstack( ( time, NR[i] ) ) )
 
 ### Plotting
 
 fig, ax  = plt.subplots( 1, 1 )
 
-ax.plot( time, NR - GR )
+for i in range( nT ):
+
+    time, GR = np.loadtxt( 'GR_{:}.dat'.format( i ) )
+    time, NR = np.loadtxt( 'NR_{:}.dat'.format( i ) )
+    ax.plot( time, NR - GR, \
+             label \
+               = r'$\theta/\pi={:}$' \
+                 .format( np.round( X2[0,ind[i],0] / np.pi, 2 ) ) )
 
 ax.grid()
+ax.legend()
 
 if zScale == 'log': ax.set_yscale( 'log' )
 
+ax.set_xlim( -1.0, 55.0 )
 ax.set_xlabel( r'$t\ \left[\mathrm{ms}\right]$' )
-ax.set_ylabel( r'$\tau_{\mathrm{NR}}-\tau_{\mathrm{GR}}$' )
+ax.set_ylabel( r'$\frac{1}{\sin\theta}\int_{R_{\mathrm{PNS}}}^{1.5\,R_{\mathrm{S}}}\left(\tau^{\theta}_{\mathrm{NR}}-\tau^{\theta}_{\mathrm{GR}}\right)\sqrt{\gamma}\,dr$' )
+ax.text( 40, 1.0e30, \
+         r'$\tau^{\theta}:=\frac{S^{2}S_{2}}{2\,D}$', fontsize = 15 )
 
-#plt.show()
+plt.show()
 
-plt.savefig( saveFigAs, dpi = 300 )
-print( '\n  Saved {:}'.format( saveFigAs ) )
+#plt.savefig( saveFigAs, dpi = 300 )
+#print( '\n  Saved {:}'.format( saveFigAs ) )
 
 plt.close()
 
